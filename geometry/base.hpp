32635b0df786ae8569ff371dc26284c00b860f16
xyz600 <xyz600600@gmail.com>
2018-12-08 18:03:14 +0900

    add line

--- geometry/base.hpp
+++ geometry/base.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cassert>
 #include <cmath>
 #include <cstdint>
 #include <functional>
 #include <initializer_list>
+#include <iostream>
 
 template <class T, std::size_t Dimension>
 struct Point
 {
 public:
-    Point() {}
+    Point()
+    {
+        std::fill(array_.begin(), array_.end(), 0);
+    }
 
     Point(std::initializer_list<T>&& list)
     {
@@ -35,7 +40,7 @@ public:
         }
     }
 
-    std::size_t size() const noexcept
+    constexpr std::size_t size() const noexcept
     {
         return Dimension;
     }
@@ -90,16 +95,6 @@ public:
     Point<T, Dimension> operator/(const T& val) const noexcept
     {
         return Apply(val, std::divides<T>());
-    }
-
-    Point<T, Dimension> Min(const Point<T, Dimension>& p) const noexcept
-    {
-        return Apply(p, std::min<T>);
-    }
-
-    Point<T, Dimension> Max(const Point<T, Dimension>& p) const noexcept
-    {
-        return Apply(p, std::max<T>);
     }
 
     T Norm2() const noexcept
@@ -172,8 +167,12 @@ private:
 template <typename T, std::size_t Dimension>
 T dot(const Point<T, Dimension>& p1, const Point<T, Dimension>& p2) noexcept
 {
-    auto tmp = p1 * p2;
-    return tmp.Reduce(std::plus<T>());
+    T ret = 0;
+    for (std::size_t i = 0; i < Dimension; i++)
+    {
+        ret += p1[i] * p2[i];
+    }
+    return ret;
 }
 
 template <typename T>
@@ -186,8 +185,75 @@ Point<T, 3> cross(const Point<T, 3>& p1, const Point<T, 3>& p2) noexcept
     };
 }
 
+template <typename T, std::size_t Dimension>
+std::ostream& operator<<(std::ostream& out, const Point<T, Dimension>& p)
+{
+    out << "(";
+    for (std::size_t i = 0; i < p.size(); i++)
+    {
+        if (i > 0)
+        {
+            out << ", ";
+        }
+        out << p[i];
+    }
+    out << ")";
+    return out;
+}
+
 using Point3D = Point<double, 3>;
 using Point2D = Point<double, 2>;
 
 using Point3I = Point<std::int64_t, 3>;
 using Point2I = Point<std::int64_t, 2>;
+
+template <typename T, std::size_t Dimension>
+struct Line
+{
+public:
+    Line() noexcept
+    {
+    }
+
+    Line(const Point<T, Dimension>& from, const Point<T, Dimension>& to) noexcept
+    {
+        for (std::size_t i = 1; i < Dimension; i += 2)
+        {
+            coef[i - 1] = to[i] - from[i];
+            coef[i] = from[i - 1] - to[i - 1];
+        }
+        if (Dimension % 2 == 1)
+        {
+            coef[Dimension - 1] = 0;
+        }
+        offset = -dot(coef, from);
+    }
+
+    Point<T, Dimension> coef;
+    T offset;
+
+    T Apply(const Point<T, Dimension>& input) const noexcept
+    {
+        return dot(coef, input) + offset;
+    }
+};
+
+template <class T, std::size_t Dimension>
+std::ostream& operator<<(std::ostream& out, const Line<T, Dimension>& line)
+{
+    out << "Line:[" << line.coef << ", " << line.offset << "]";
+    return out;
+}
+
+using Line3D = Line<double, 3>;
+using Line2D = Line<double, 2>;
+
+Point2D Intersect(const Line2D& l1, const Line2D& l2)
+{
+    const double denom = l1.coef[0] * l2.coef[1] - l1.coef[1] * l2.coef[0];
+    assert(denom != 0.0);
+
+    const double num0 = -l1.offset * l2.coef[1] + l1.coef[1] * l2.offset;
+    const double num1 = l1.offset * l2.coef[0] - l1.coef[0] * l2.offset;
+    return Point2D({ num0 / denom, num1 / denom });
+}
--- geometry/test/base_test.cpp
+++ geometry/test/base_test.cpp
@@ -1,10 +1,15 @@
 #include "base.hpp"
 
 #include <gtest/gtest.h>
+#include <sstream>
+
+// test Point
+
+constexpr double eps = 1e-7;
 
 TEST(point_test, geometry_base_test)
 {
-    Point3D p1 = { 1.0, 2.0, 4.0 };
+    Point3D p1({ 1.0, 2.0, 4.0 });
 
     ASSERT_EQ(p1.size(), 3);
     ASSERT_EQ(p1[0], 1.0);
@@ -14,8 +19,8 @@ TEST(point_test, geometry_base_test)
 
 TEST(point_operator_plus, geometry_base_test)
 {
-    Point3D p1 = { 1.0, 2.0, 4.0 };
-    Point3D p2 = { 8.0, 16.0, 32.0 };
+    Point3D p1({ 1.0, 2.0, 4.0 });
+    Point3D p2({ 8.0, 16.0, 32.0 });
 
     auto p3 = p1 + p2;
 
@@ -84,23 +89,6 @@ TEST(point_operator_div, geometry_base_test)
     ASSERT_EQ(p4[2], 2.0);
 }
 
-TEST(point_min_max, geometry_base_test)
-{
-    Point3D p1 = { 1.0, 2.0, 4.0 };
-    Point3D p2 = { 4.0, 1.0, 5.0 };
-
-    auto pmin = p1.Min(p2);
-
-    ASSERT_EQ(pmin[0], 1.0);
-    ASSERT_EQ(pmin[1], 1.0);
-    ASSERT_EQ(pmin[2], 4.0);
-
-    auto pmax = p1.Max(p2);
-    ASSERT_EQ(pmax[0], 4.0);
-    ASSERT_EQ(pmax[1], 2.0);
-    ASSERT_EQ(pmax[2], 5.0);
-}
-
 TEST(point_dot, geometry_base_test)
 {
     Point3D p1 = { 1.0, 2.0, 4.0 };
@@ -123,15 +111,46 @@ TEST(point_cross, geometry_base_test)
 
 TEST(point_normalize, geometry_base_test)
 {
-    Point3D p = { 1.0, 2.0, 2.0 };
+    Point3D p({ 1.0, 2.0, 2.0 });
 
     ASSERT_EQ(p.Norm2(), 9.0);
 
     auto p2 = Point3D({ 1.0, 2.0, 2.0 }).Normalize();
-    constexpr double eps = 1e-7;
 
     ASSERT_TRUE(abs(1.0 - p2.Norm2()) < eps);
     ASSERT_TRUE(abs(1.0 / 3.0 - p2[0]) < eps);
     ASSERT_TRUE(abs(2.0 / 3.0 - p2[1]) < eps);
     ASSERT_TRUE(abs(2.0 / 3.0 - p2[2]) < eps);
+}
+
+TEST(point_ostream, geometry_base_test)
+{
+    std::stringstream ss;
+
+    Point3I p({ 1, 2, 4 });
+    ss << p;
+    ASSERT_EQ(ss.str(), "(1, 2, 4)");
+}
+
+// test Line
+
+TEST(line_constructor, geometry_base_test)
+{
+    const Point2D p1({ 0.0, 1.0 });
+    const Point2D p2({ 2.0, 5.0 });
+
+    const Point2D coef = p2 - p1;
+    Line2D line(p1, p2);
+
+    ASSERT_TRUE(abs(dot(coef, line.coef)) < eps);
+}
+
+TEST(line_intersect, geometry_base_test)
+{
+    const Line2D line1(Point2D({ 10.0, 0.0 }), Point2D({ 0.0, 10.0 }));
+    const Line2D line2(Point2D({ 0.0, 0.0 }), Point2D({ 10.0, 10.0 }));
+
+    auto inter = Intersect(line1, line2);
+    ASSERT_EQ(inter[0], 5.0);
+    ASSERT_EQ(inter[1], 5.0);
 }
