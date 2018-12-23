0b6f64bbf92a85d24d7dc6d9725da5a6e4d5aca4
xyz600 <xyz600600@gmail.com>
2018-12-23 20:40:06 +0900

    ノードを追加する形式に変更

--- graph/base.hpp
+++ graph/base.hpp
@@ -14,32 +14,28 @@ public:
     using EdgeType = E;
     using NeighborListType = std::unordered_set<std::size_t>;
 
-    SparseGraph(const std::size_t size, bool undirected = true) noexcept
-        : neighbor_list_(size)
-        , undirected_(undirected)
-        , nodes_(size)
+    SparseGraph(bool undirected = true) noexcept
+        : undirected_(undirected)
     {
+    }
+
+    void push_node(NodeType n) noexcept
+    {
+        nodes_.push_back(n);
+        neighbor_list_.emplace_back();
     }
 
     void connect(const std::size_t from, const std::size_t to, const EdgeType edge) noexcept
     {
-        const std::size_t new_size = std::max(from, to) + 1;
-
-        if (neighbor_list_.size() < new_size)
-        {
-            neighbor_list_.resize(new_size);
-            nodes_.resize(new_size);
-        }
-
         if (undirected_)
         {
             neighbor_list_[from].insert(to);
+            neighbor_list_[to].insert(from);
             edges_[std::make_pair(std::min(from, to), std::max(from, to))] = edge;
         }
         else
         {
             neighbor_list_[from].insert(to);
-            neighbor_list_[to].insert(from);
             edges_[std::make_pair(from, to)] = edge;
         }
     }
@@ -49,12 +45,12 @@ public:
         if (undirected_)
         {
             neighbor_list_[from].erase(to);
+            neighbor_list_[to].erase(from);
             edges_.erase(std::make_pair(std::min(from, to), std::max(from, to)));
         }
         else
         {
             neighbor_list_[from].erase(to);
-            neighbor_list_[to].erase(from);
             edges_.erase(std::make_pair(from, to));
         }
     }
@@ -109,11 +105,15 @@ public:
     using EdgeType = E;
     using NeighborListType = std::vector<std::size_t>;
 
-    DenseGraph(const std::size_t size, bool undirected = true) noexcept
-        : neighbor_list_(size)
-        , undirected_(undirected)
-        , nodes_(size)
+    DenseGraph(bool undirected = true) noexcept
+        : undirected_(undirected)
     {
+    }
+
+    void push_node(NodeType n) noexcept
+    {
+        nodes_.push_back(n);
+        neighbor_list_.emplace_back();
     }
 
     void connect(const std::size_t from, const std::size_t to, const EdgeType edge) noexcept
@@ -129,12 +129,12 @@ public:
         if (undirected_)
         {
             neighbor_list_[from].push_back(to);
+            neighbor_list_[to].push_back(from);
             edges_[std::make_pair(std::min(from, to), std::max(from, to))] = edge;
         }
         else
         {
             neighbor_list_[from].push_back(to);
-            neighbor_list_[to].push_back(from);
             edges_[std::make_pair(from, to)] = edge;
         }
     }
@@ -144,12 +144,12 @@ public:
         if (undirected_)
         {
             neighbor_list_[from].erase(std::find(neighbor_list_[from].begin(), neighbor_list_[from].end(), to));
+            neighbor_list_[to].erase(std::find(neighbor_list_[to].begin(), neighbor_list_[to].end(), from));
             edges_.erase(std::make_pair(std::min(from, to), std::max(from, to)));
         }
         else
         {
             neighbor_list_[from].erase(std::find(neighbor_list_[from].begin(), neighbor_list_[from].end(), to));
-            neighbor_list_[to].erase(std::find(neighbor_list_[to].begin(), neighbor_list_[to].end(), from));
             edges_.erase(std::make_pair(from, to));
         }
     }
