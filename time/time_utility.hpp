5147882418a6424a1d3ff782282b769372fd5453
xyz600 <xyz600600@gmail.com>
2019-01-27 13:50:48 +0900

    add time getter

--- time/time_utility.hpp
+++ time/time_utility.hpp
@@ -9,3 +9,13 @@ uint64_t getCycle()
                      : "=a"(low), "=d"(high));
     return ((uint64_t)low) | ((uint64_t)high << 32);
 }
+
+uint64_t getMillisecTime(const uint64_t start_clock)
+{
+    return (getCycle() - start_clock) * 1000ull / CLOCKS_PER_SEC;
+}
+
+uint64_t getMicrosecTime(const uint64_t start_clock)
+{
+    return (getCycle() - start_clock) * 1000000ull / CLOCKS_PER_SEC;
+}
