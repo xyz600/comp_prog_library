a02b3239451af6fc7073a5c88f7288a25bf8809d
xyz600 <xyz600600@gmail.com>
2019-05-05 20:21:34 +0900

    add union find

--- graph/union_find.hpp
+++ graph/union_find.hpp
@@ -1,27 +1,32 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 
 struct UnionFind
 {
-    std::vector<std::size_t> par;
+    std::vector<std::size_t> _par;
+    std::vector<std::size_t> _size;
+    std::vector<std::size_t> _rank;
 
     UnionFind(std::size_t N)
-        : par(N)
+        : _par(N)
+        , _size(N, 1)
+        , _rank(N, 0)
     {
         for (std::size_t i = 0; i < N; i++)
         {
-            par[i] = i;
+            _par[i] = i;
         }
     }
 
     std::size_t Root(std::size_t x)
     {
-        if (par[x] == x)
+        if (_par[x] == x)
         {
             return x;
         }
-        return par[x] = Root(par[x]);
+        return _par[x] = Root(_par[x]);
     }
 
     void Unite(std::size_t x, std::size_t y)
@@ -30,8 +35,26 @@ struct UnionFind
         auto ry = Root(y);
         if (rx != ry)
         {
-            par[rx] = ry;
+            if (_rank[rx] < _rank[ry])
+            {
+                _par[rx] = ry;
+                _size[ry] += _size[rx];
+            }
+            else
+            {
+                _par[ry] = rx;
+                _size[rx] += _size[ry];
+                if (_rank[rx] == _rank[ry])
+                {
+                    _rank[ry]++;
+                }
+            }
         }
+    }
+
+    size_t Size(std::size_t x)
+    {
+        return _size[Root(x)];
     }
 
     bool Same(std::size_t x, std::size_t y)
