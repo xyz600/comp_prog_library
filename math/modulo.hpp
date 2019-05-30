7701576bc84b20d49c2465d39d46cc913282c09c
xyz600 <xyz600600@gmail.com>
2019-05-30 22:32:23 +0900

    update modulo to recieve value

--- math/modulo.hpp
+++ math/modulo.hpp
@@ -18,6 +18,15 @@ public:
     ModuloInteger operator*(const ModuloInteger& value);
     void operator*=(const ModuloInteger& value);
 
+    ModuloInteger operator+(const std::size_t value);
+    void operator+=(const std::size_t value);
+
+    ModuloInteger operator-(const std::size_t value);
+    void operator-=(const std::size_t value);
+
+    ModuloInteger operator*(const std::size_t value);
+    void operator*=(const std::size_t value);
+
     std::size_t value;
 };
 
@@ -55,4 +64,40 @@ template <std::size_t MOD>
 void ModuloInteger<MOD>::operator*=(const ModuloInteger& value)
 {
     this->value = (this->value * value.value) % MOD;
+}
+
+template <std::size_t MOD>
+ModuloInteger<MOD> ModuloInteger<MOD>::operator+(const std::size_t value)
+{
+    return (this->value + value) % MOD;
+}
+
+template <std::size_t MOD>
+void ModuloInteger<MOD>::operator+=(const std::size_t value)
+{
+    this->value = (this->value + value) % MOD;
+}
+
+template <std::size_t MOD>
+ModuloInteger<MOD> ModuloInteger<MOD>::operator-(const std::size_t value)
+{
+    return (this->value + MOD - value) % MOD;
+}
+
+template <std::size_t MOD>
+void ModuloInteger<MOD>::operator-=(const std::size_t value)
+{
+    this->value = (this->value - value) % MOD;
+}
+
+template <std::size_t MOD>
+ModuloInteger<MOD> ModuloInteger<MOD>::operator*(const std::size_t value)
+{
+    return (this->value * value) % MOD;
+}
+
+template <std::size_t MOD>
+void ModuloInteger<MOD>::operator*=(const std::size_t value)
+{
+    this->value = (this->value * value) % MOD;
 }
