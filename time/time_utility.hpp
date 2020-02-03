8aade1115cc24e8b5c545601a5b28c83dd562737
xyz600 <xyz600600@gmail.com>
2020-02-03 12:43:13 +0900

    fix time utility

--- time/time_utility.hpp
+++ time/time_utility.hpp
@@ -2,20 +2,20 @@
 #include <ctime>
 
 // clock -> sec は、CLOCKS_PER_SEC
-uint64_t getCycle()
+int64_t getCycle()
 {
     uint32_t low, high;
     __asm__ volatile("rdtsc"
                      : "=a"(low), "=d"(high));
-    return ((uint64_t)low) | ((uint64_t)high << 32);
+    return ((int64_t)low) | ((int64_t)high << 32);
 }
 
-uint64_t getMillisecTime(const uint64_t start_clock)
+int64_t getMillisecTime(const int64_t start_clock)
 {
     return (getCycle() - start_clock) * 1000ull / CLOCKS_PER_SEC;
 }
 
-uint64_t getMicrosecTime(const uint64_t start_clock)
+int64_t getMicrosecTime(const int64_t start_clock)
 {
     return (getCycle() - start_clock) * 1000000ull / CLOCKS_PER_SEC;
 }
